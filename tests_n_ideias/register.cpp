dc3848e3fec8c277a046b139f533a5a59998e392
Joao Pedro <blvck.joao37@gmail.com>
2026-03-15 22:46:16 -0300

    Initial commit

---
+++ tests_n_ideias/register.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <memory>
+
+#define SIZE_32 32
+#define SIZE_8 8
+
+class Register {
+
+};
+
+class Reg32 : public Register {
+        private:
+                int size;
+                const std::string name;
+                std::unique_ptr<bool> data;
+
+        public:
+
+                Reg32(std::string n) : size(SIZE_32), name(n) {
+                        data = std::make_unique<bool>(SIZE_32);
+                }
+
+                
+};
+
+
+class Reg32 {
+        private:
+                const char* name
+                int 
+
+        public:
+                
+};
+
+class Reg8 {
+        public:
+                char data;
+}
